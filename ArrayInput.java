04ec150d5b6804e47d7b429dec748ec5b7601158
vedants27 <vedantss27@gmail.com>
2025-04-18 17:53:23 +0530

    Added method to input integer array.

--- ArrayInput.java
+++ ArrayInput.java
@@ -3,5 +3,19 @@ import java.util.*;
 
 class ArrayInput {
     Scanner scan = new Scanner(System.in);
+
+    // Method to take user input for an array
+int[] getUserArray() {
+    System.out.println("Enter the size of the array:");
+    int size = scan.nextInt();
+    int[] array = new int[size];
+
+    System.out.println("Enter " + size + " numbers:");
+    for (int i = 0; i < size; i++) {
+        array[i] = scan.nextInt();
+    }
+    return array;
+}
+
 }
 
