0d4556bb7578855656111347ab0402fcc66da3fd
vedants27 <vedantss27@gmail.com>
2025-04-18 17:54:27 +0530

    Added method to input ArrayList from user.

--- ArrayInput.java
+++ ArrayInput.java
@@ -15,6 +15,16 @@ int[] getUserArray() {
         array[i] = scan.nextInt();
     }
     return array;
+} 
+    // Method to take user input for an ArrayList
+ArrayList<Integer> getUserArrayList() {
+    ArrayList<Integer> arrayList = new ArrayList<>();
+    System.out.println("Enter numbers for ArrayList (type 'done' to finish):");
+
+    while (scan.hasNextInt()) {
+        arrayList.add(scan.nextInt());
+    }
+    return arrayList;
 }
 
 }
