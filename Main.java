4e283e79550caec9b3275de12f7f74f23ec2383f
vedants27 <vedantss27@gmail.com>
2025-04-18 17:42:17 +0530

    Update Main.java file by Adding main class with menu and input setup.

--- Main.java
+++ Main.java
@@ -1,1 +1,23 @@
+// Name - Vedant Shitole
+// Prn - 23070126143
+// Batch - Aiml B3 2023-2027
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        ArrayInput arrayInput = new ArrayInput();
+        ArrayFunctions functions = new ArrayFunctions();
+
+        while (true) {
+            System.out.println("\nSelect an operation:");
+            System.out.println("1. Separate Even and Odd numbers\n2. Find index of closest neighboring numbers\n3. Convert Array to ArrayList\n4. Convert ArrayList to Array\n0. Exit");
+            int choice = scanner.nextInt();
+            
+            switch (choice) {
+                // Cases added in later commits
+            }
+        }
+    }
+}
 
