0f047254927cfb086f13b4ce55c840c158487bc2
vedants27 <vedantss27@gmail.com>
2025-04-18 17:56:57 +0530

    Added method to separate even and odd numbers.

--- ArrayFunctions.java
+++ ArrayFunctions.java
@@ -6,5 +6,21 @@ class ArrayFunctions {
     void display(ArrayList<Integer> array) {
         System.out.println("Array is: " + array);
     }
+    void separateOddEven(int[] array) {
+    ArrayList<Integer> even = new ArrayList<Integer>();
+    ArrayList<Integer> odd = new ArrayList<Integer>();
+    for (int i = 0; i < array.length; i++) {
+        if (array[i] % 2 == 0) {
+            even.add(array[i]);
+        } else {
+            odd.add(array[i]);
+        }
+    }
+    System.out.print("Even ");
+    display(even);
+    System.out.print("Odd ");
+    display(odd);
+}
+
 }
 
